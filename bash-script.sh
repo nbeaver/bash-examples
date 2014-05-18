dc87b9f305af2eb319c406885dd40abd6b41e201
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-05-18 07:48:26 -0500

    Added some informating about bash arguments (May 12).

--- bash-script.sh
+++ bash-script.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# $# # number of arguments
+# $* # all arguments
+# $@ # all arguments
+# http://stackoverflow.com/questions/12314451/accessing-bash-command-line-args-vs
+# http://www.gnu.org/software/bash/manual/bashref.html#Special-Parameters
+# $0 # script name
+# $1 # 1st argument, last name
+# $2 # 2nd argument, first name
+# $9 # ninth argument
+
 # Treat unset variables and parameters other than the special parameters "@" and "*" as an error when performing parameter  expansion.
 set -u
 # set -o nonunset # longer version
