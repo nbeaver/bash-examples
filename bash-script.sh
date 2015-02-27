e1cea13dbdeb975cda2469f9c7789b1bf5ce3002
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-02-26 22:50:15 -0600

    Made the script executable and added an example text file.

--- bash-script.sh
+++ bash-script.sh
@@ -12,7 +12,11 @@
 
 # Treat unset variables and parameters other than the special parameters "@" and "*" as an error when performing parameter  expansion.
 set -u
-# set -o nounset # longer version
+set -o nounset # equivalent longer version
+
+# Now undo that.
+set +u
+set +o nounset
 
 # Terminate as soon as any command fails.
 set -e
@@ -24,36 +28,54 @@ set -o pipefail
 set -euo pipefail
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 
+# Undo all at the same time.
+set +euo pipefail
+
 # Testing if a file exists.
 test -e myfile.txt
 
 # Testing a command's return value and exiting if there is an error.
-mycommand
-ERROR_CODE=$?
-if [ $ERROR_CODE -ne 0 ]; then
-    echo "Error: command failed."
-    exit $ERROR_CODE
-fi
+example_error() {
+    return 1;
+}
+check_exit_code() {
+    $* # run the command.
+    ERROR_CODE=$?
+    if [ $ERROR_CODE -ne 0 ]; then
+        echo "The command '"$*"' failed with return code $ERROR_CODE."
+    fi
+}
+check_exit_code example_error
 
 # Testing piped commands for errors.
-command1 | command2
-TEMP=("${PIPESTATUS[@]}")
-echo "TEMP: ${TEMP[@]}"
-if [ ${TEMP[0]} -ne 0 ]; then
-    echo "1st command error: ${TEMP[0]}"
-elif [ ${TEMP[1]} -ne 0 ]; then
-    echo "2nd command error: ${TEMP[1]}"
-else
+another_error() {
+    return 42;
+}
+print_pipe_errors() {
+    printf "Running this:\n\t$*\n"
+    $*
+    TEMP=("${PIPESTATUS[@]}")
+    declare -p TEMP
     echo "TEMP: ${TEMP[@]}"
-    echo "Both return codes = 0."
-fi
+    if [ ${TEMP[0]} -ne 0 ]; then
+        echo "1st command error: ${TEMP[0]}"
+        echo "2nd command error: ${TEMP[1]}"
+    elif [ ${TEMP[1]} -ne 0 ]; then
+        echo "2nd command error: ${TEMP[1]}"
+    else
+        echo "TEMP: ${TEMP[@]}"
+        echo "Both return codes = 0."
+    fi
+}
+print_pipe_errors 'example_error | another_error'
+print_pipe_errors echo "Hello, world." | tr . !
+
 # https://stackoverflow.com/questions/6565694/left-side-failure-on-pipe-in-bash/6566171
 # Yes, that really is the best way to copy an array.
 
 # Using the if construct with arithmetic conditionals.
 if [ $# -lt 1 ]; then
     echo "No arguments."
-    exit
 fi
 
 # Test if a string is not empty.
@@ -61,6 +83,7 @@ VAR="hello"
 if [ -n "$VAR" ]; then
     echo "VAR is not empty"
 fi
+
 # Test if a string is empty.
 VAR=""
 if [ -z "$VAR" ]; then
@@ -68,80 +91,107 @@ if [ -z "$VAR" ]; then
 fi
 # http://timmurphy.org/2010/05/19/checking-for-empty-string-in-bash/
 
-# Using the if construct with process return values.
-if ping -c 1 google.com > /dev/null; then
-	wget http://www.google.com
-else
-	echo 'Cannot ping google.com.'
-fi
-# Alternative syntax.
-if ping -c 1 google.com > /dev/null
-then
-	wget http://www.google.com
-else
-	echo 'Cannot ping google.com.'
-fi
+VAR=1
+# Seeing a variable's value.
+declare -p VAR
+echo $VAR
+
+## Using the if construct with process return values.
+#if ping -c 1 google.com > /dev/null; then
+#	echo 'Succesfully pinged google.com.'
+#else
+#	echo 'Cannot ping google.com.'
+#fi
+## Alternative syntax that is more symmetric.
+#if ping -c 1 google.com > /dev/null
+#then
+#	echo 'Succesfully pinged google.com.'
+#else
+#	echo 'Cannot ping google.com.'
+#fi
 
 # Integer comparison in if statements
 # eq, ne, gt, ge, lt, le
 # ==, !=, > , >=, < , <=
 
 # http://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
-for myfile in *.ext
+for myfile in *.txt
 do
+    echo "Filename with extension: $myfile"
     myfile_no_extension="${myfile%.*}"
-    echo "$myfile"
+    echo "Filename without extension: $myfile_no_extension"
 done
 
 # iterate over arguments separated by spaces.
 # http://stackoverflow.com/questions/255898/how-to-iterate-over-arguments-in-bash-script
 for var in "$@"
 do
-	echo "$var"
+    echo "$var"
 done
 
 # Run through each line of a text file.
 # http://stackoverflow.com/questions/1521462/looping-through-the-content-of-a-file-in-bash
-while read line; do
-	echo "$line"
-done < ~/path/to/file.txt
+while read MYVAR
+do
+    echo "$MYVAR" #MYVAR holds the line.
+done < ./example.txt
 
 # bash functions.
 function myfunc {
-	local num_args="$#"
-	echo "number of args: $num_args"
+local num_args="$#"
+echo "number of args: $num_args"
+declare -p num_args
 }
 # invoke the function
-myfunc
+myfunc 1 2 3
 
 # Read-only (immutable or constant) variables.
-readonly mypath="/tmp/"
+readonly readonly_path="/tmp/"
+declare -p readonly_path
 
 # Unfortunately, these are inconvenient for shell functions,
 # because even if they're local,
 # they still can only be assigned once,
 # and so this function can only be run once:
 function myfunc {
-	readonly local mypath="/tmp/"
-	echo "$mypath"
+readonly local readonly_local_path="/tmp/"
+echo "$readonly_local_path"
+declare -p readonly_path
 }
 
 # read a text file into a shell variable.
-TARGET=$(cat -- "myfile.txt")
+TARGET=$(cat -- "./example.txt")
 
 # If you cd to a relative path,
 # make sure to do this, or the directory might be wrong.
+echo $CDPATH
 unset CDPATH
+echo $CDPATH
 # https://bosker.wordpress.com/2012/02/12/bash-scripters-beware-of-the-cdpath/
 
 # Splitting piplines with comments on each line.
-cat myfile.txt | # Output the file.
-sort -n |        # Sort it numerically.
-uniq |           # Remove repeats.
-head -n 1        # Get the line beginning with the first (smallest) number.
+cat ./example.txt | # Output the file.
+sort -n |           # Sort it numerically.
+uniq |              # Remove repeats.
+head -n 1           # Get the line beginning with the first (smallest) number.
 
-# Find directory script was called from.
+# Find directory script was called from, even if it's called from a symlink.
 DIR="$(dirname "$0")"
-# more robust:
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+declare -p DIR
+# more robust for e.g. calling from a symlink:
+FULL_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+declare -p FULL_PATH
 # https://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+
+# Brace expansion
+echo -n '{aa,bb,cc,dd} = '
+echo {aa,bb,cc,dd} # aa bb cc dd
+echo -n '{0..12} ='
+echo {0..12}       # 0 1 2 3 4 5 6 7 8 9 10 11 12
+echo -n '{3..-2} ='
+echo {3..-2}       # 3 2 1 0 -1 -2
+echo -n '{a..g} ='
+echo {a..g}        # a b c d e f g
+echo -n '{g..a} ='
+echo {g..a}        # g f e d c b a
+# http://www.linuxjournal.com/content/bash-brace-expansion
